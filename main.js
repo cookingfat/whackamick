b007a93771f75a305b165b0d97bbd91060d8bae9
cookingfat <120741930+cookingfat@users.noreply.github.com>
2023-01-05 12:01:45 +0000

    Update main.js

--- main.js
+++ main.js
@@ -9,30 +9,82 @@ function run(){
     const i = Math.floor(Math.random() * holes.length)
     const hole = holes[i]
     let timer = null
+    let scoreTimer = null
 
+    // Introduce a new variable to determine whether to use a bomb or a mole
+    const useBomb = Math.random() < 0.2
     const img = document.createElement('img')
-    img.classList.add('mole')
-    img.src = 'assets/mole.png'
 
-    img.addEventListener('click', () => {
-        score += 10
-        sound.play()
-        scoreEl.textContent = score
-        img.src = 'assets/mole-whacked.png'
-        clearTimeout(timer)
-        setTimeout(() => {
-            hole.removeChild(img)
-            run()
-        }, 500)
-    })
+    // Set a new variable indicating whether the current image is a mole or a bomb
+    const isMole = !useBomb
+
+    if (useBomb) {
+        // Use the bomb image
+        img.classList.add('bomb')
+        img.src = 'assets/bomb.png'
+
+
+        // Add an event listener to the bomb that calls gameOver when clicked
+        img.addEventListener('click', gameOver)
+    } else {
+        // Use the mole image
+        img.classList.add('mole')
+        img.src = 'assets/mole.png'
+
+        // Add a clicked property to the mole image and set it to false
+        img.clicked = false
+
+        // Add an event listener to the mole that increments the score when clicked
+        img.addEventListener('click', () => {
+            // Only increment the score if the clicked property is not true
+            if (!img.clicked) {
+                score += 10
+                sound.play()
+                scoreEl.textContent = score
+                img.src = 'assets/mole-whacked.png'
+                img.clicked = true // Set the clicked property to true
+                clearTimeout(timer)
+                clearTimeout(scoreTimer) // Clear the score timer
+                setTimeout(() => {
+                    hole.removeChild(img)
+                    run()
+                }, 500)
+            }
+        })
+    }
 
     hole.appendChild(img)
 
+    // Only set a score timer if the current image is a mole
+    if (isMole) {
+        // Set a score timer that decreases the score by 10 every 1000 milliseconds (1 second)
+        scoreTimer = setInterval(() => {
+            score -= 10
+            scoreEl.textContent = score
+        }, 1000)
+    }
+
     timer = setTimeout(() => {
         hole.removeChild(img)
+        clearInterval(scoreTimer) // Clear the score timer
         run()
     }, 1500)
 }
+
+// Define the gameOver function
+function gameOver() {
+    // Display a game over message
+    alert('Game Over!')
+
+    // Reset the score to 0
+    score = 0
+    scoreEl.textContent = score
+
+    // Clear any timeouts or intervals to stop the game
+    clearTimeout(timer)
+    clearInterval(scoreTimer)
+}
+
 run()
 
 window.addEventListener('mousemove', e => {
